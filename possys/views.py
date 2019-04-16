f861fbc964f05704beb5b0604d2ee30c45674035
fazerog02 <fazerog02@gmail.com>
2019-04-16 17:40:08 +0900

    [add]signup page

--- possys/views.py
+++ possys/views.py
@@ -5,12 +5,12 @@ from django.contrib.auth import login
 # Create your views here.
 
 def Signup(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = SignupForm(data=request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("/")
+            return redirect("/possys/") #とりあえずトップに飛ばす
     else:
         form = SignupForm()
-        return render(request, 'possys/signup.html', {'form': form, })
+    return render(request, 'possys/signup.html', {'form': form, })
