88e352ce6028027ce0c1ad2c2b3463752ba915e9
sanoto <kmn2wei@yahoo.co.jp>
2019-04-26 17:26:54 +0900

    [add] 購入・チャージ機能追加

--- possys/views.py
+++ possys/views.py
@@ -1,8 +1,49 @@
 from django.shortcuts import render
 from .models import Product, Transaction
+from users.models import Card
 from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import BasePermission, IsAuthenticated
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class IsPossysHousing(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name='possys').exists()
+
+
+def add_transaction(price, idm, product=None):
+    card = Card.objects.filter(idm=idm).first()
+    if card is None:
+        return Response('IDm is not found', status=418)
+
+    transaction = Transaction(
+        price=price,
+        user=card.user,
+        product=product
+    )
+    transaction.save()
+    return Response('True')
+
+
+@login_required
+@permission_classes(IsPossysHousing)
+@api_view(['POST'])
+def add_transaction_with_product(request, price, idm, product_id):
+    product = Product.objects.filter(pk=product_id).first()
+    if product is None:
+        return Response('Product is not found', status=418)
+    return add_transaction(price, idm, product)
+
+
+@login_required
+@permission_classes(IsPossysHousing)
+@api_view(['POST'])
+def add_transaction_without_product(request, price, idm):
+    return add_transaction(price, idm)
 
 
 def products_list(request):
