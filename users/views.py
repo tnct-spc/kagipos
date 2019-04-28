d6c202026721742818cdb0f6ce830ab7f27fc98a
fazerog02 <fazerog02@gmail.com>
2019-04-28 22:41:35 +0900

    [change]rename users/form.py -> users/forms.py

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .form import SignupForm
+from .forms import SignupForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 
