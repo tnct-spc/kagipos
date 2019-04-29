73616fd80303995f9facf1e1ffe61c283db8ab9d
sanoto <kmn2wei@yahoo.co.jp>
2019-04-29 19:40:12 +0900

    [fix] add_transaction_with_productの引数にpriceはいらなかった

--- possys/urls.py
+++ possys/urls.py
@@ -21,7 +21,7 @@ from . import views as possys_views
 urlpatterns = [
     path('', possys_views.products_list, name="products_list"),
     path('history/', possys_views.history, name="history"),
-    path('api/add_transaction/<int:price>/<idm>/<int:product_id>/', possys_views.add_transaction_with_product),
+    path('api/add_transaction/<idm>/<int:product_id>/', possys_views.add_transaction_with_product),
     path('api/add_transaction/<int:price>/<idm>/', possys_views.add_transaction_without_product),
 ]
 
