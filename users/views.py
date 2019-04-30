7896a021155e65b96558aa7d748fb70bed22b8ba
fazerog02 <fazerog02@gmail.com>
2019-04-30 12:47:42 +0900

    [fix]SignupViewをより簡略化

--- users/views.py
+++ users/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from django.views import View
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from possys.views import add_transaction
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Temporary, Card
+from .models import Temporary, Card, User
 
 
 @api_view(['GET'])
@@ -20,22 +19,18 @@ def add_idm(request, idm):
     return Response(temporary.uuid.hex)
 
 
-class SignupView(View):
+class SignupView(CreateView):
+    model = User
     form_class = SignupForm
     template_name = 'possys/signup.html'
+    success_url = '/possys/'
 
-    def get(self, request):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form, })
-
-    def post(self, request):
-        form = self.form_class(data=request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            # とりあえずトップに飛ばす
-            return redirect('/possys/')
-        return render(request, 'possys/signup.html', {'form': form, })
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        new_user = authenticate(username=username, password=password)
+        login(self.request, new_user)
+        return valid
 
 
 class CardCreateView(LoginRequiredMixin, CreateView):
